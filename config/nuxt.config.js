ad78921bc03a6124da5b0b41d3b70b16f5797f37
kapellan221089 <serg221089@gmail.com>
2020-10-04 15:36:01 +0300

    start

---
+++ config/nuxt.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    css: [
+        'assets/main.css'
+    ],
+    head: {
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+        ]
+    },
+    telemetry: true
+    /*modules: [
+        '@nuxtjs/axios'
+    ]*/
+}
---
+++ index.js
@@ -0,0 +1,21 @@
+const app = require('express')();
+const express = require('express');
+const {Nuxt, Builder}  = require('nuxt');
+const config = require('./config/nuxt.config.js');
+const morgan = require('morgan');
+
+const nuxt = new Nuxt(config);
+const builder = new Builder(nuxt);
+
+app.use(morgan('dev'));
+
+
+builder.build()
+  .then(() => {
+  // Рендерить каждый маршрут с Nuxt.js
+    app.use(nuxt.render)
+    // Запустить сервер
+    app.listen(6000, ()=>{
+       console.log('Server run in 6000 port');
+    });
+  });
